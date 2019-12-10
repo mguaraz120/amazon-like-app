0ee59c538cafe71a766b39ec863a8a62d265ee0b
Mario Vizcaino <vizhar1952@gmail.com>
2019-12-10 15:39:41 -0500

    Created the functions needed to run the customer side app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,14 +27,66 @@ function itemsForSale(){
         head: header,
         colWidths: [20, 55, 10, 20]
     });
-        var item_ids = [];
-    for (var i = 0; i < result.length; i++) {
+        let item_ids = [];
+    for (let i = 0; i < result.length; i++) {
         item_ids.push(result[i].item_id);
-        table.push([result[i].item_id, result[i].price.toFixed(2), result[i].department_name]);
+        table.push([result[i].item_id, result[i].product_name, result[i].price.toFixed(2), result[i].department_name]);
     }
-        var output = table.toString();
+        let output = table.toString();
         console.log(output);
         purchaseItem(item_ids);
 
     });
+}
+
+function purchaseItem(list){
+    inquirer
+        .prompt([{
+            name: "buy",
+            type: "list",
+            message: "Which item would you like to purchase?",
+            choices: list
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Enter quantity"
+        }
+    ])
+    .then(function(answer){
+        let query = "SELECT item_id, stock_quantity, price FROM products WHERE ?";
+        connection.query(query, {item_id: answer.buy}, function(err, res){
+            let inputQuantity = answer.quantity;
+            checkStock(res[0].stock_quantity, inputQuantity, res[0].price.toFixed(2), res[0].item_id)
+        });
+    })
+}
+
+function checkStock(on_stock, buy_quantity, price, item_id) {
+    if (on_stock >= buy_quantity) {
+        let total_price = buy_quantity * price;
+        console.log(`Your total amount is $${total_price}.\nThank you for your purchase on BAMAZON!`);
+        // updates database
+        updateStock(buy_quantity, item_id);
+    } else {
+        console.log(`Insufficient quantity on stock!\nOnly ${on_stock} items on stock!`);
+        connection.end();
+    }
+}
+
+function updateStock(quantity, item_id) {
+    var query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE ?";
+    connection.query(
+        query,
+        [
+            quantity,
+            {
+                item_id: item_id
+            }
+        ],
+        function (error) {
+            if (error) throw error;
+            console.log("DB was succefully updated!");
+            connection.end();
+        });
 }
