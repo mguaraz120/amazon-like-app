7a96c921137ebdb6270610f9af728c67bbcc0c79
Mario Vizcaino <vizhar1952@gmail.com>
2019-12-11 17:51:37 -0500

    Working on the items for sale

--- bamazonManager.js
+++ bamazonManager.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     // sets username
     user: "root",
     // sets password
-    password: "",
+    password: "admin",
     // sets current database
     database: "bamazon_db"
 });
@@ -41,6 +41,7 @@ function chooseMenu()
             switch (answer.menu)
             {
                 case "View Products for Sale":
+                    itemsForSale();
                     break;
                 case "View Low Inventory":
                     break;  
@@ -52,4 +53,42 @@ function chooseMenu()
                     break;
             }
         })
+}
+function itemsForSale(func)
+{
+    connection.query(
+        "SELECT item_id, products_name, price, stock_quantity, department_name " +
+        "FROM products " +
+        "WHERE price > 0;", function(err, result)
+        {
+            if (err) throw err;
+            let selection = result[0];
+            let header = [];
+            for (let product in selection)
+            {
+               header.push(product);
+            } 
+            let table = new Table(
+            {
+                head: header,
+                colWidths: [15, 55, 10, 5, 20]
+            });
+            let item_ids = [];
+            for (let i = 0; i < result.length; i++) 
+            {
+                item_ids.push(result[i].item_id);
+                table.push([result[i].item_id, result[i].product_name, result[i].price.toFixed(2), result[i].stock_quantity, 
+                result[i].department_name]);
+            }
+            let output = table.toString();
+            console.log(output);
+            if (func)
+            {
+                func(item_ids);
+            }
+            else
+            {
+                chooseMenu();
+            }
+        });
 }
